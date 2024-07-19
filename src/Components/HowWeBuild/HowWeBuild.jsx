e80b3047a3fb483e127a6e63523334c19c1b526a
ali nabizadeh <alinabizadeh1913@gmail.com>
2024-07-20 01:08:30 +0330

    start HowWeBuild.jsx

---
+++ src/Components/HowWeBuild/HowWeBuild.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const HowWeBuild = () => {
+    return (
+        <section id="how-we-build" className="my-5">
+
+        </section>
+    )
+}
+
+export default HowWeBuild;
